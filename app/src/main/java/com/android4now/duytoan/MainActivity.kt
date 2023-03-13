9389b8df8d313f4a4fad6d3fb494c4a7546676f7
Nguyen Duy Toan <duytoan.ng@samsung.com>
2023-03-13 09:28:25 +0700

    initial commit

---
+++ app/src/main/java/com/android4now/duytoan/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.android4now.duytoan
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.android4now.duytoan.ui.theme.Android4nowTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Android4nowTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Android4nowTheme {
+        Greeting("Android")
+    }
+}
